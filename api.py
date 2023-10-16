d15183112244ff8d86492bfcc64795ba7f7ff994
Pelumi Adeboye <oluwapelumiadeboye@gmail.com>
2023-10-16 13:59:58 +0100

    Add files via upload

--- api.py
+++ api.py
@@ -12,7 +12,7 @@ import torch.nn as nn
 from pydantic import BaseModel
 import torchvision.models as models
 import faiss
-# import classify_images
+
 import numpy as np
 
 
@@ -56,8 +56,10 @@ class FeatureExtractor(nn.Module):
         self.decoder = decoder
 
     def forward(self, image):
-        x = self.main(image)
-        return x
+        features = self.resnet50(image)
+        features = features.view(features.size(0), -1)
+        features = self.feature_fc(features)
+        return features
 
     def predict(self, image):
         with torch.no_grad():
@@ -74,8 +76,12 @@ except:
     raise OSError("No Feature Extraction model found. Check that you have the decoder and the model in the correct location")
 
 try:
-    with open('index.pickle', 'rb') as handle:
-        index = pickle.load(handle)
+    with open('file_ids2.pkl', 'rb') as handle2:
+        ids = pickle.load(handle2)
+
+    # Load the Faiss index using Faiss's built-in function
+    index = faiss.read_index('index2.pkl')
+
 
     pass
 except:
@@ -96,34 +102,36 @@ def healthcheck():
 def predict_image(image: UploadFile = File(...)):
     pil_image = Image.open(image.file)
 
-    processed_image = ImageProcessor(pil_image)
-    image_embedding = FeatureExtractor(processed_image)
-
+    image_processor = ImageProcessor()
+    processed_image = image_processor(pil_image)
+    image_embedding = feature_model.predict(processed_image)
+    
+    image_embedding_numpy = image_embedding.detach().numpy()
 
     return JSONResponse(content={
-    "Features": image_embedding, # Return the image embeddings here
+    "Features": image_embedding_numpy.tolist(),  # Convert to a JSON-serializable format
+    })
+
     
-        })
   
 @app.post('/predict/similar_images')
-def predict_combined(image: UploadFile = File(...), text: str = Form(...)):
-    print(text)
-    pil_image = Image.open(image.file)
+def predict_combined(image: UploadFile = File(...)):
     
-    processed_image = ImageProcessor(pil_image)
-    image_embeddings = FeatureExtractor(processed_image)
+    pil_image = Image.open(image.file)
 
-    image_ids = list(index.keys())
-    all_embeddings = list(index.values())
+    image_processor = ImageProcessor()
+    processed_image = image_processor(pil_image)
+    image_embeddings = feature_model.predict(processed_image)
+
 
     n_neighbors = 10
     D, I = index.search(np.array(image_embeddings, dtype=np.float32), n_neighbors)
-    similar_index = [all_embeddings[i] for i in I[0]]
+    similar_index = [ids[i] for i in I[0]] #ids not embeddings
 
     return JSONResponse(content={
     "similar_index": similar_index, # Return the index of similar images here
         })
     
-    
+
 if __name__ == '__main__':
-  uvicorn.run(app, host="127.0.0.1", port=8080)
+  uvicorn.run("api:app", host="0.0.0.0", port=8080)
